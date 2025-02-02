f255ebefd9b219f1f7f9675b3513f2fa32fba5d6
sriram2511 <sriramchitra25@gmail.com>
2025-02-02 09:58:29 +0530

    init

---
+++ hooks/useFetch.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function useFetch(url: string) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [url]);
+
+  return { data, loading, error };
+}
